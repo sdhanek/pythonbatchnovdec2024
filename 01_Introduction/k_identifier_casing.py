fea0a795a06632cf6cdeb77ccc319402c938b17f
sdhanek <sindhudhanekula03@gmail.com>
2024-11-18 06:25:05 +0000

    class03 - Introduction & Arithemetic operations

---
+++ 01_Introduction/k_identifier_casing.py
@@ -0,0 +1,13 @@
+
+#Python is a case sensitive language 
+animal = "DOG"
+print(animal)
+#print(Animal) is not defined
+
+#Snake casing
+student = "shiva"
+employee_salary = 234.34
+
+#Camel casing
+Student = "Shiva"
+EmployeeSalary = 234.34
---
+++ 01_Introduction/l_multiline_statements.py
@@ -0,0 +1,5 @@
+"""
+Purpose
+"""
+#Max 79 characters in line 
+
